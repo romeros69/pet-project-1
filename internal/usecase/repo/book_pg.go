409703fb763012df9a8376056fb960fa2520096a
romeros69 <jefopc@gmail.com>
2022-10-17 22:45:28 +0300

    feat: add repo func GetBooks

---
+++ internal/usecase/repo/book_pg.go
@@ -0,0 +1,41 @@
+package repo
+
+import (
+	"context"
+	"pet-project-1/internal/entity"
+	"pet-project-1/pkg/postgres"
+	"fmt"
+)
+
+type BookRepo struct {
+	pg *postgres.Postgres
+}
+
+func NewBookRepo(pg *postgres.Postgres) *BookRepo {
+	return &BookRepo{
+		pg: pg,
+	}
+}
+func (b *BookRepo) GetBooks(ctx context.Context) ([]entity.Book, error) {
+	query := `SELECT * from books`
+
+	rows, err := b.pg.Pool.Query(ctx, query)
+	if err != nil {
+		return nil, fmt.Errorf("cannot execute query: %w", err)
+	}
+	defer rows.Close()
+
+	var books []entity.Book
+
+	for rows.Next() {
+		var book entity.Book
+		err = rows.Scan(&book.ID,
+			&book.Tittle,
+			&book.Author)
+		if err != nil {
+			return nil, fmt.Errorf("error in parsing book: %w", err)
+		}
+		books = append(books, book)
+	}
+	return books, nil
+}
